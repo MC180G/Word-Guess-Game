ee0246f81a41d836b15c2d4b4bdfec25f3549db3
Matt Colson <mc180g@gmail.com>
2020-04-15 11:16:49 -0400

    Centered divs, dld placeholder image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,176 +1,170 @@
 var wordGuessGame = {
 
-  wordsToPick: {
-    douglas: { picture: "mikedouglas.jpg" },
-    keaton: { picture: "mikekeaton.jpg" },
-    caine: { picture: "mikecaine.jpg" },
-    jackson: { picture: "mikejackson.jpg" },
-    vick: { picture: "mikevick.jpg" },
-    jordan: { picture: "mikejordan.jpg" },
-    stipe: { picture: "mikestipe.jpg" },
-    bolton: { picture: "mikebolton.jpg" },
-    shannon: { picture: "mikeshannon.jpg" },
-    richards: { picture: "mikerichards.jpg" },
-    bay: { picture: "mikebay.jpg" },
-    imperioli: { picture: "mikeimp.jpg" },
-    landon: { picture: "mikelandon.jpg" },
-    cera: {picture: "mikecera.jpg" },
-    che: {picture:"mikeche.jpg" }
-  },
+    wordsToPick: {
+        douglas: { picture: "mikedouglas.jpg" },
+        keaton: { picture: "mikekeaton.jpg" },
+        caine: { picture: "mikecaine.jpg" },
+        jackson: { picture: "mikejackson.jpg" },
+        vick: { picture: "mikevick.jpg" },
+        jordan: { picture: "mikejordan.jpg" },
+        stipe: { picture: "mikestipe.jpg" },
+        bolton: { picture: "mikebolton.jpg" },
+        shannon: { picture: "mikeshannon.jpg" },
+        richards: { picture: "mikerichards.jpg" },
+        bay: { picture: "mikebay.jpg" },
+        imperioli: { picture: "mikeimp.jpg" },
+        landon: { picture: "mikelandon.jpg" },
+        cera: { picture: "mikecera.jpg" },
+        che: { picture: "mikeche.jpg" }
+    },
 
-  targetWord: null,
-  wordLetters: [],
-  correctLetters: [],
-  usedLetters: [],
-  guessesLeft: 0,
-  totalGuesses: 0,
-  letterUsed: null,
-  wins: 0,
+    targetWord: null,
+    wordLetters: [],
+    correctLetters: [],
+    usedLetters: [],
+    guessesLeft: 0,
+    totalGuesses: 0,
+    letterUsed: null,
+    wins: 0,
 
-  setupGame: function () {
+    setupGame: function() {
 
-    // eliminate picture from previous round
-    
-    // Choose a Micheal
-    var objKeys = Object.keys(this.wordsToPick);
-    this.targetWord = objKeys[Math.floor(Math.random() * objKeys.length)];
+        // eliminate picture from previous round
 
-    //Divide up into letters
+        // Choose a Micheal
+        var objKeys = Object.keys(this.wordsToPick);
+        this.targetWord = objKeys[Math.floor(Math.random() * objKeys.length)];
 
-    this.wordLetters = this.targetWord.split("");
+        //Divide up into letters
 
-    //Lays out blank word as dashes
+        this.wordLetters = this.targetWord.split("");
 
-    this.rebuildWordView();
+        //Lays out blank word as dashes
 
-    // user Guesses
+        this.rebuildWordView();
 
-    this.processUpdateTotalGuesses();
-  },
+        // user Guesses
 
-  //User guesses
+        this.processUpdateTotalGuesses();
+    },
 
-  updatePage: function (letter) {
+    //User guesses
 
-    if (this.guessesLeft === 0) {
-      this.restartGame();
-    } 
-    
-    else {
+    updatePage: function(letter) {
 
-      this.updateGuesses(letter);
+        if (this.guessesLeft === 0) {
+            this.restartGame();
+        } else {
 
-      this.updateMatchedLetters(letter);
+            this.updateGuesses(letter);
 
-      this.rebuildWordView();
+            this.updateMatchedLetters(letter);
 
-      if (this.updateWins() === true) {
-        this.restartGame();
-      }
+            this.rebuildWordView();
+
+            if (this.updateWins() === true) {
+                this.restartGame();
+            }
+        }
+    },
+
+    // If user guesses wrong...
+
+    updateGuesses: function(letter) {
+
+        if ((this.usedLetters.indexOf(letter === -1) && this.wordLetters.indexOf(letter) === -1)) {
+
+            this.usedLetters.push(letter);
+
+            this.guessesLeft--;
+
+            document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+            document.querySelector("#guessed-letters").innerHTML = this.usedLetters.join(", ");
+        }
+    },
+
+    processUpdateTotalGuesses: function() {
+
+        this.totalGuesses = this.wordLetters.length + 5;
+        this.guessesLeft = this.totalGuesses;
+
+        document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+    },
+
+    updateMatchedLetters: function(letter) {
+
+        for (var i = 0; i < this.targetWord.length; i++) {
+
+            if ((letter === this.wordLetters[i]) && (this.correctLetters.indexOf(letter) === -1)) {
+                this.correctLetters.push(letter);
+            }
+
+        }
+
+    },
+
+    rebuildWordView: function() {
+
+        var wordView = "";
+
+        for (var i = 0; i < this.targetWord.length; i++) {
+
+            if (this.correctLetters.indexOf(this.wordLetters[i]) !== -1) {
+                wordView += this.wordLetters[i];
+            } else { wordView += "&nbsp;_&nbsp;"; }
+
+        }
+
+        document.querySelector("#current-word").innerHTML = wordView;
+    },
+
+    restartGame: function() {
+        document.querySelector("#guessed-letters").innerHTML = "";
+        this.targetWord = null;
+        this.wordLetters = [];
+        this.correctLetters = [];
+        this.usedLetters = [];
+        this.guessesLeft = 0;
+        this.totalGuesses = 0;
+        this.letterUsed = null;
+        this.setupGame();
+        this.rebuildWordView();
+    },
+
+    updateWins: function() {
+        var win;
+
+        if (this.correctLetters.length === 0) {
+            win = false;
+        } else { win = true }
+
+        for (var i = 0; i < this.targetWord.length; i++) {
+            if (this.correctLetters.indexOf(this.wordLetters[i]) === -1) {
+                win = false
+            }
+        }
+        if (win) {
+            this.wins = this.wins + 1;
+
+            document.querySelector("#wins").innerHTML = this.wins;
+
+
+
+            document.querySelector("#mike-div").innerHTML =
+                "<img id='mike-image' src='assets/images/" +
+                this.wordsToPick[this.targetWord].picture + "'>";
+
+            return true;
+        }
+        return false;
     }
-  },
-
-  // If user guesses wrong...
-
-  updateGuesses: function (letter) {
-
-    if ((this.usedLetters.indexOf(letter === -1) && this.wordLetters.indexOf(letter) === -1)) {
-
-      this.usedLetters.push(letter);
-
-      this.guessesLeft--;
-
-      document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
-      document.querySelector("#guessed-letters").innerHTML = this.usedLetters.join(", ");
-    }
-  },
-
-  processUpdateTotalGuesses: function () {
-
-    this.totalGuesses = this.wordLetters.length + 5;
-    this.guessesLeft = this.totalGuesses;
-
-    document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
-  },
-
-  updateMatchedLetters: function (letter) {
-
-    for (var i = 0; i < this.targetWord.length; i++) {
-
-      if ((letter === this.wordLetters[i]) && (this.correctLetters.indexOf(letter) === -1)) {
-        this.correctLetters.push(letter);
-      }
-
-    }
-
-  },
-
-  rebuildWordView: function () {
-
-    var wordView = "";
-
-    for (var i = 0; i < this.targetWord.length; i++) {
-
-      if (this.correctLetters.indexOf(this.wordLetters[i]) !== -1) {
-        wordView += this.wordLetters[i];
-      } else { wordView += "&nbsp;_&nbsp;"; }
-
-    }
-
-    document.querySelector("#current-word").innerHTML = wordView;
-  },
-
-  restartGame: function () {
-    document.querySelector("#guessed-letters").innerHTML = "";
-    this.targetWord = null;
-    this.wordLetters = [];
-    this.correctLetters = [];
-    this.usedLetters = [];
-    this.guessesLeft = 0;
-    this.totalGuesses = 0;
-    this.letterUsed = null;
-    this.setupGame();
-    this.rebuildWordView();
-  },
-
-  updateWins: function () {
-    var win;
-
-    if (this.correctLetters.length === 0) {
-      win = false;
-    } else { win = true }
-
-    for (var i = 0; i < this.targetWord.length; i++) {
-      if (this.correctLetters.indexOf(this.wordLetters[i]) === -1) {
-        win = false
-      }
-    }
-    if (win) {
-      this.wins = this.wins + 1;
-
-      document.querySelector("#wins").innerHTML = this.wins;
-
-
-
-      document.querySelector("#mike-div").innerHTML =
-        "<img id='mike-image' src='assets/images/" +
-        this.wordsToPick[this.targetWord].picture + "' alt='" +
-        this.wordsToPick[this.targetWord].song + "'>";
-
-      var audio = new Audio(this.wordsToPick[this.targetWord].preview);
-      audio.play();
-
-      return true;
-    }
-    return false;
-  }
 };
 
 wordGuessGame.setupGame();
 
-document.onkeyup = function (event) {
-  if (event.keyCode >= 49 && event.keyCode <= 90) {
-    wordGuessGame.letterUsed = event.key.toLowerCase();
-    wordGuessGame.updatePage(wordGuessGame.letterUsed);
-  }
+document.onkeyup = function(event) {
+    if (event.keyCode >= 49 && event.keyCode <= 90) {
+        wordGuessGame.letterUsed = event.key.toLowerCase();
+        wordGuessGame.updatePage(wordGuessGame.letterUsed);
+    }
 };
